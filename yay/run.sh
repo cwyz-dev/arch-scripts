9e3e0da67c24d4ea03ba52f275317c8f033b6f5d
cwyz-dev <chris@wwwoods.xyz>
2026-05-30 11:53:48 -0600

    Removing silencing

--- yay/run.sh
+++ yay/run.sh
@@ -1,6 +1,6 @@
 # Taken from u/merith-tk (www.reddit.com/r/archlinux/comments/fg05b1/comment/fk2yohv/) on 2026-05-29
 
-git clone https://aur.archlinux.org/yay-bin.git >/dev/null 2>&1
+git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
-sudo pacman -S base-devel --noconfirm --needed >/dev/null 2>&1
-makepkg -s >/dev/null 2>&1
+sudo pacman -S base-devel --noconfirm --needed
+makepkg -s
