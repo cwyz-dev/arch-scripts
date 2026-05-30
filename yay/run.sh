e9b395fdfb955afce82f8298d660506b700e42ae
cwyz-dev <chris@wwwoods.xyz>
2026-05-29 19:04:51 -0600

    Ensuring packages, and silent script mode

--- yay/run.sh
+++ yay/run.sh
@@ -1,7 +1,6 @@
 # Taken from u/merith-tk (www.reddit.com/r/archlinux/comments/fg05b1/comment/fk2yohv/) on 2026-05-29
 
-git clone https://aur.archlinux.org/yay-bin.git
+git clone https://aur.archlinux.org/yay-bin.git >/dev/null 2>&1
 cd yay-bin
-makepkg -s
-cd ..
-rm -rf yay-bin
+sudo pacman -S base-devel --noconfirm --needed >/dev/null 2>&1
+makepkg -s >/dev/null 2>&1
